0da2ae1bdb81e7083c2e53443e437c5e3cbcc65b
Vinesh Pendyala <vinesh.r.pendyala@gmail.com>
2019-06-03 21:03:28 -0400

    initial commit

--- server.js
+++ server.js
@@ -7,20 +7,17 @@ const PORT = process.env.PORT || 3000
 
 const app = express()
 
-// Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"))
 
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(methodOverride("_method"));
 
-// Set Handlebars.
 const exphbs = require("express-handlebars")
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }))
 app.set("view engine", "handlebars")
 
-// Import routes and give the server access to them.
 let routes = require("./controllers/burgers_controller.js")
 
 app.use(routes)
