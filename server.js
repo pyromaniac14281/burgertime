00994666b195d926974964140f652f76bcb39977
Vinesh Pendyala <vinesh.r.pendyala@gmail.com>
2019-06-04 00:52:03 -0400

    .json

--- server.js
+++ server.js
@@ -1,27 +1,23 @@
-const express = require("express")
-const bodyParser = require("body-parser")
-const methodOverride = require("method-override")
+const express = require("express");
+const bodyParser = require("body-parser");
 
+const PORT = process.env.PORT || 8080;
 
-const PORT = process.env.PORT || 3000
+const app = express();
 
-const app = express()
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-app.use(express.static("public"))
+const exphbs = require("express-handlebars");
 
-app.use(bodyParser.urlencoded({ extended: false }))
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
-app.use(methodOverride("_method"));
+const routes = require("./controllers/burgers_controller");
 
-const exphbs = require("express-handlebars")
-
-app.engine("handlebars", exphbs({ defaultLayout: "main" }))
-app.set("view engine", "handlebars")
-
-let routes = require("./controllers/burgers_controller.js")
-
-app.use(routes)
+app.use(routes);
 
 app.listen(PORT, function() {
-    console.log("App now listening at localhost:" + PORT)
-})
+  console.log("Server listening on: http://localhost:" + PORT);
+});
